c3e9f7895156488e3351e1b9671bd17323607e1e
Caca <2402008@students.poltas.ac.id>
2026-07-02 11:43:22 +0700

    menambahkan struktur awal proyek web

---
+++ script.js
@@ -0,0 +1,6 @@
+const deskripsi = document.getElementById("deskripsi");
+const btnUbah = document.getElementById("btnUbah");
+
+btnUbah.addEventListener("click", function () {
+    deskripsi.textContent = "Deskripsi berhasil diubah menggunakan JavaScript.";
+});
